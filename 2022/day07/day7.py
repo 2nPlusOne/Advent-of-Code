681448466e1519c80078f94c037dd46f0df8e2aa
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2022-12-28 23:34:17 -0500

    Day 7 part 2

--- 2022/day07/day7.py
+++ 2022/day07/day7.py
@@ -76,14 +76,14 @@ def process_input(inputFile: str):
                     currDir = currDir.get_child(line[2])
         return root
 
-def sum_small_directories(dir: Directory, maxDirSize: int) -> int:
+def part1(dir: Directory, maxDirSize: int) -> int:
     """Part 1: Sum the directories in the filesystem whose size is 
     less than or equal to 100000."""
     total = 0
     
     # Recursively visit all child directories
     for child in dir.get_children():
-        total += sum_small_directories(child, maxDirSize)
+        total += part1(child, maxDirSize)
     
     # If the current directory's size is less than or equal 
     # to the max, add it to the total
@@ -93,12 +93,35 @@ def sum_small_directories(dir: Directory, maxDirSize: int) -> int:
         
     return total
 
-def part2(data: str) -> int:
-    """>.<"""  
-    pass
+def part2(root: Directory) -> int:
+    """Finds the size of the smallest directory which, if deleted, would
+    make enough space for the update (30,000,000 bytes) to be installed."""  
+    total_space = 70000000
+    required_by_update = 30000000
+    # Calculate free space by subtracting total used from total space
+    free_space = total_space - root.get_size()
+    # Calculate space need by subtracting free space from the amount required by update
+    remainder_needed = required_by_update - free_space
+    
+    # Iterate over every directory to find the answer
+    smallest = root.get_size()
+    stack = root.get_children()
+    while stack != []:
+        dir = stack.pop()
+        dir_size = dir.get_size()
+        
+        # check current size
+        if remainder_needed <= dir_size < smallest:
+            smallest = dir_size
+            
+        # add all children to stack
+        for child in dir.get_children(): 
+            stack.append(child)
+            
+    return smallest
 
 if __name__ == "__main__":
     filename = os.path.join(os.path.dirname(__file__), 'input.txt')
     root = process_input(filename)
-    print(f"Part 1: {sum_small_directories(root, 100000)}")
-    # print(f"Part 2: {part2(root)}")
+    print(f"Part 1: {part1(root, 100000)}")
+    print(f"Part 2: {part2(root)}")
