4c7e32bd0cf82f981d07d8e86a5af449ddba0746
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2022-12-29 19:01:55 -0500

    Day 9 part 2

--- 2022/day09/day9.py
+++ 2022/day09/day9.py
@@ -11,7 +11,7 @@ class Position:
     def is_adjacent(self, other_pos: Position):
         return abs(other_pos.x - self.x) < 2 and abs(other_pos.y - self.y) < 2
     
-    def get_tuple(self): return (self.x, self.y)
+    def as_tuple(self): return (self.x, self.y)
 
 def process_input(inputFile: str) -> List[str]:
     moves = []
@@ -29,7 +29,7 @@ def part1(moves: list[str]) -> int:
     tail = Position(0,0)
     
     positions = set()
-    positions.add(tail.get_tuple())
+    positions.add(tail.as_tuple())
     
     for move in moves:
         # move the head
@@ -55,16 +55,56 @@ def part1(moves: list[str]) -> int:
             elif head.x - tail.x < 0:
                 tail.x -= 1
                 
-            positions.add(tail.get_tuple()) # tuples are hashable into the set
+            positions.add(tail.as_tuple()) # tuples are hashable into the set
             
     return len(positions)
 
-def part2():
-    """>.<"""  
-    pass
+def part2(moves: list[str], n: int) -> int:
+    """Count the number of unique positions visited by the tail 
+    after the sequence of movements (the input). Part 2 extends
+    the length of the rope to have n segments."""
+    segments = [Position(0,0) for _ in range(n)]
+    head = segments[0]
+    tail = segments[-1]
+    
+    positions = set()
+    positions.add(tail.as_tuple())
+    
+    for move in moves:
+        # move the head
+        if move == 'U':
+            head.y += 1
+        elif move == 'D':
+            head.y -= 1
+        elif move == 'L':
+            head.x -= 1
+        elif move == 'R':
+            head.x += 1
+        
+        # iterate over all segments after the head
+        for i in range(1, len(segments)):
+            segment = segments[i]
+            prev_segment = segments[i-1]
+            
+            # only move a segment if it's not adjacent to the previous segment        
+            if not segment.is_adjacent(prev_segment):
+                # handle moving the segment towards the previous one
+                if prev_segment.y - segment.y > 0:
+                    segment.y += 1
+                elif prev_segment.y - segment.y < 0:
+                    segment.y -= 1
+                    
+                if prev_segment.x - segment.x > 0:
+                    segment.x += 1
+                elif prev_segment.x - segment.x < 0:
+                    segment.x -= 1
+                
+        positions.add(tail.as_tuple()) # tuples are hashable into the set
+            
+    return len(positions)
 
 if __name__ == "__main__":
     filename = os.path.join(os.path.dirname(__file__), 'input.txt')
     moves = process_input(filename)
     print(f"Part 1: {part1(moves)}")
-    # print(f"Part 2: {part2()}")
+    print(f"Part 2: {part2(moves, 10)}")
